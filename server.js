bfeaa0737efe98ff43419dd73b5edba49f3f4f29
Khuynh <huynhquangkieu@gmail.com>
2023-01-01 21:30:14 +0700

    disable cart auth

--- server.js
+++ server.js
@@ -104,31 +104,31 @@ server.post('/auth/login', (req, res) => {
   res.status(200).json({ access_token, "userInfo": { "username": userJustLogin.username, email, "id": userJustLogin.id } })
 })
 
-server.use(/^\/carts\/[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/, (req, res, next) => {
-  if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
-    const status = 401
-    const message = 'Error in authorization format'
-    res.status(status).json({ status, message })
-    return
-  }
-  try {
-    let verifyTokenResult;
-    verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
-    console.log("🚀 ~ file: server.js:105 ~ server.use ~ verifyTokenResult", verifyTokenResult)
+// server.use(/^\/carts\/[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/, (req, res, next) => {
+//   if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
+//     const status = 401
+//     const message = 'Error in authorization format'
+//     res.status(status).json({ status, message })
+//     return
+//   }
+//   try {
+//     let verifyTokenResult;
+//     verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
+//     console.log("🚀 ~ file: server.js:105 ~ server.use ~ verifyTokenResult", verifyTokenResult)
 
-    if (verifyTokenResult instanceof Error) {
-      const status = 401
-      const message = 'Access token not provided'
-      res.status(status).json({ status, message })
-      return
-    }
-    next()
-  } catch (err) {
-    const status = 401
-    const message = 'Error access_token is revoked'
-    res.status(status).json({ status, message })
-  }
-})
+//     if (verifyTokenResult instanceof Error) {
+//       const status = 401
+//       const message = 'Access token not provided'
+//       res.status(status).json({ status, message })
+//       return
+//     }
+//     next()
+//   } catch (err) {
+//     const status = 401
+//     const message = 'Error access_token is revoked'
+//     res.status(status).json({ status, message })
+//   }
+// })
 
 server.use(express.static('assets'));
 
